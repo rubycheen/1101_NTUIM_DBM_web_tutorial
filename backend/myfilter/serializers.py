177a30a8d85ccc141bbd6ea7a9cf6edb63230ce6
b05303124 <b05303124@ntu.edu.tw>
2021-10-17 13:45:13 +0800

    update readme

--- backend/myfilter/serializers.py
+++ backend/myfilter/serializers.py
@@ -5,15 +5,3 @@ class StationListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Station
         fields = '__all__'
-
-# class TimetableSearchSerializer(serializers.serializer):
-#     start_station = serializers.CharField(max_length=100)
-#     end_station = serializers.CharField(max_length=100)
-#     outward_search_date = serializers.CharField(max_length=100)
-#     outward_search_time = serializers.CharField(max_length=100)
-
-#     def validate(self, data):
-#         """
-#         Check search time is after current time
-#         """
-#         search_time = 
